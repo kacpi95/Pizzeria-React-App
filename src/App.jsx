6980c2de855ba0f5acc1fabe4f9be5d1f58b6a59
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-06-22 19:15:02 +0200

    Add navigation structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router';
+import Home from './components/pages/Home/Home';
+import NoPage from './components/pages/NoPage/NoPage';
+import Table from './components/pages/Table/Table';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/table/:id' element={<Table />} />
+      <Route path='*' element={<NoPage />} />
+    </Routes>
+  );
 }
 
 export default App;
