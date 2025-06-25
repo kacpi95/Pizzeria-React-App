6eb6ef7bb4307a12169eee5d127265d588713893
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-06-25 13:47:46 +0200

    Fix navigation Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import NoPage from './components/pages/NoPage/NoPage';
 import Table from './components/pages/Table/Table';
--- src/components/views/NavBar/NavBar.jsx
+++ src/components/views/NavBar/NavBar.jsx
@@ -1,8 +1,9 @@
-import { NavLink, Nav } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <div className='d-flex justify-content-between algin-items-center px-4 py-3 mt-2 bg-primary text-white rounded'>
+    <div className='d-flex justify-content-between align-items-center px-4 py-3 mt-2 bg-primary text-white rounded'>
       <h3 className='m-0'>Waiter.App</h3>
       <Nav>
         <Nav.Link as={NavLink} to='/' className='text-white'>
