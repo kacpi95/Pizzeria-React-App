b449b0b2ebcf6839c6c18b0fbb37febe3d581f02
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-06-26 01:16:21 +0200

    Fix server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export const API_URL =
-  typeof window !== 'undefined' && window.location.hostname.includes('replit')
-    ? `https://${window.location.hostname}/tables`
-    : import.meta.env.MODE === 'production'
+  import.meta.env.MODE === 'production'
     ? '/tables'
     : 'http://localhost:3131/tables';
